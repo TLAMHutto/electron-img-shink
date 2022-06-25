56e7fb1cae72a035dca4242790b8d548aec169fa
TLAMHutto <keatonhutto8@gmail.com>
2022-06-24 21:34:18 -0500

    stuff

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function createMainWindow() {
     title: 'ImageShrink',
     width: isDev ? 800 : 500,
     height: 600,
-    icon: `${__dirname}/assets/icons/icon.png`,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     resizable: isDev ? true : false,
     backgroundColor: 'white',
     webPreferences: {
@@ -41,7 +41,7 @@ function createAboutWindow() {
     title: 'About ImageShrink',
     width: 300,
     height: 300,
-    icon: `${__dirname}/assets/icons/icon.png`,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     resizable: false,
     backgroundColor: 'white',
   })
